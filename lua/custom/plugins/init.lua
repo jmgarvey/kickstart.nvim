42df0e34372a7aa7d777d69319cc36e2f2792511
Jared Garvey <jared@medra.ai>
2025-11-28 19:48:35 -0800

    fix venv

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -32,11 +32,14 @@ return {
       'mfussenegger/nvim-dap-python', --optional
       { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
     },
-    lazy = false,
-    branch = 'regexp', -- This is the regexp branch, use this for the new version
-    config = function()
-      require('venv-selector').setup()
-    end,
+    opts = {
+      name = 'venv',
+      auto_refresh = false,
+      lazy = false,
+    },
+    --config = function()
+    --  require('venv-selector').setup()
+    --end,
     keys = {
       { ',v', '<cmd>VenvSelect<cr>' },
     },
