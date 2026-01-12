ed36439bfe07a55a64053659bc87599ccc630781
Jared Garvey <jared@medra.ai>
2026-01-11 22:32:52 -0800

    functioning debugger

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -1,181 +1,115 @@
--- debug.lua
---
--- Shows how to use the DAP plugin to debug your code.
---
--- Primarily focused on configuring the debugger for Go, but can
--- be extended to other languages as well. That's why it's called
--- kickstart.nvim and not kitchen-sink.nvim ;)
-
+-- Docs at https://github.com/mfussenegger/nvim-dap-python are useful.
 return {
+  -- keep-sorted start block=yes
+
   {
-    -- NOTE: Yes, you can install new plugins here!
     'mfussenegger/nvim-dap',
-    -- NOTE: And you can specify dependencies as well
-    dependencies = {
-      -- Creates a beautiful debugger UI
-      'rcarriga/nvim-dap-ui',
-
-      -- Required dependency for nvim-dap-ui
-      'nvim-neotest/nvim-nio',
-
-      -- Installs the debug adapters for you
-      'williamboman/mason.nvim',
-      'jay-babu/mason-nvim-dap.nvim',
-
-      -- Add your own debuggers here
-      'HiPhish/debugpy.nvim',
-      'mfussenegger/nvim-dap-python',
-      'leoluz/nvim-dap-go',
-    },
+    lazy = true,
+    -- Copied from LazyVim/lua/lazyvim/plugins/extras/dap/core.lua and
+    -- modified.
     keys = {
-      -- Basic debugging keymaps, feel free to change to your liking!
       {
-        '<F5>',
-        function()
-          require('dap').continue()
-        end,
-        desc = 'Debug: Start/Continue',
-      },
-      {
-        '<F1>',
-        function()
-          require('dap').step_into()
-        end,
-        desc = 'Debug: Step Into',
-      },
-      {
-        '<F2>',
-        function()
-          require('dap').step_over()
-        end,
-        desc = 'Debug: Step Over',
-      },
-      {
-        '<F3>',
-        function()
-          require('dap').step_out()
-        end,
-        desc = 'Debug: Step Out',
-      },
-      {
-        '<leader>b',
+        '<leader>db',
         function()
           require('dap').toggle_breakpoint()
         end,
-        desc = 'Debug: Toggle Breakpoint',
+        desc = 'Toggle Breakpoint',
       },
+
       {
-        '<leader>B',
+        '<leader>dc',
         function()
-          require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+          require('dap').continue()
         end,
-        desc = 'Debug: Set Breakpoint',
+        desc = 'Continue',
       },
-      -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
+
       {
-        '<F7>',
+        '<leader>dC',
         function()
-          require('dapui').toggle()
+          require('dap').run_to_cursor()
         end,
-        desc = 'Debug: See last session result.',
+        desc = 'Run to Cursor',
+      },
+
+      {
+        '<leader>dT',
+        function()
+          require('dap').terminate()
+        end,
+        desc = 'Terminate',
       },
     },
-    config = function()
-      local dap = require 'dap'
-      local dapui = require 'dapui'
-
-      require('mason-nvim-dap').setup {
-        -- Makes a best effort to setup the various debuggers with
-        -- reasonable debug configurations
-        automatic_installation = true,
-
-        -- You can provide additional configuration to the handlers,
-        -- see mason-nvim-dap README for more information
-        handlers = {},
-
-        -- You'll need to check that you have the required things installed
-        -- online, please don't ask me how to install them :)
-        ensure_installed = {
-          -- Update this to ensure that you have the debuggers for the langs you want
-          'delve',
-          'python',
-        },
-      }
-
-      -- Dap UI setup
-      -- For more information, see |:help nvim-dap-ui|
-      dapui.setup {
-        -- Set icons to characters that are more likely to work in every terminal.
-        --    Feel free to remove or use ones that you like more! :)
-        --    Don't feel like these are good choices.
-        icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
-        controls = {
-          icons = {
-            pause = '⏸',
-            play = '▶',
-            step_into = '⏎',
-            step_over = '⏭',
-            step_out = '⏮',
-            step_back = 'b',
-            run_last = '▶▶',
-            terminate = '⏹',
-            disconnect = '⏏',
-          },
-        },
-      }
-
-      -- Change breakpoint icons
-      -- vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
-      -- vim.api.nvim_set_hl(0, 'DapStop', { fg = '#ffcc00' })
-      -- local breakpoint_icons = vim.g.have_nerd_font
-      --     and { Breakpoint = '', BreakpointCondition = '', BreakpointRejected = '', LogPoint = '', Stopped = '' }
-      --   or { Breakpoint = '●', BreakpointCondition = '⊜', BreakpointRejected = '⊘', LogPoint = '◆', Stopped = '⭔' }
-      -- for type, icon in pairs(breakpoint_icons) do
-      --   local tp = 'Dap' .. type
-      --   local hl = (type == 'Stopped') and 'DapStop' or 'DapBreak'
-      --   vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
-      -- end
-
-      dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-      dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-      dap.listeners.before.event_exited['dapui_config'] = dapui.close
-
-      -- Install golang specific config
-      require('dap-go').setup {
-        delve = {
-          -- On Windows delve must be run attached or it crashes.
-          -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
-          detached = vim.fn.has 'win32' == 0,
-        },
-      }
-    end,
+    dependencies = {
+      {
+        'igorlfs/nvim-dap-view',
+        -- Alternative UI.  https://igorlfs.github.io/nvim-dap-view/keymaps
+        -- Load with :DapViewOpen
+        -- Use g? in windows to see keymaps.
+        ---@module 'dap-view'
+        ---@type dapview.Config
+        opts = {},
+      },
+    },
   },
-  --{
-  --  'jay-babu/mason-nvim-dap.nvim',
-  --  ---@type MasonNvimDapSettings
-  --  opts = {
-  --    -- This line is essential to making automatic installation work
-  --    -- :exploding-brain
-  --    handlers = {},
-  --    automatic_installation = {
-  --      -- These will be configured by separate plugins.
-  --      exclude = {
-  --        'delve',
-  --        'python',
-  --      },
-  --    },
-  --    -- DAP servers: Mason will be invoked to install these if necessary.
-  --    ensure_installed = {
-  --      'bash',
-  --      'codelldb',
-  --      'php',
-  --      'python',
-  --    },
-  --  },
-  --  dependencies = {
-  --    'mfussenegger/nvim-dap',
-  --    'williamboman/mason.nvim',
-  --  },
-  --:w
-  --},
+  {
+    'rcarriga/nvim-dap-ui',
+    config = true,
+    keys = {
+      {
+        '<leader>du',
+        function()
+          require('dapui').toggle {}
+        end,
+        desc = 'Dap UI',
+      },
+    },
+    dependencies = {
+      -- keep-sorted start block=yes
+      {
+        'jay-babu/mason-nvim-dap.nvim',
+        ---@type MasonNvimDapSettings
+        opts = {
+          -- This line is essential to making automatic installation work
+          -- :exploding-brain
+          handlers = {},
+          automatic_installation = false,
+          -- DAP servers: these will be installed by mason-tool-installer.nvim
+          -- for consistency.
+          ensure_installed = {},
+        },
+        dependencies = {
+          'mfussenegger/nvim-dap',
+          'mason-org/mason.nvim',
+        },
+      },
+      {
+        'mfussenegger/nvim-dap-python',
+        lazy = true,
+        config = function()
+          -- debugpy is installed in my standard Python virtualenv so that it's
+          -- available with all the other modules.
+          require('dap-python').setup '~/.pyenv/shims/python'
+          -- require('dap-python').setup '~/.pyenv/versions/medra/bin/python'
+        end,
+        -- Consider the mappings at
+        -- https://github.com/mfussenegger/nvim-dap-python?tab=readme-ov-file#mappings
+        dependencies = {
+          'mfussenegger/nvim-dap',
+        },
+      },
+      {
+        'nvim-neotest/nvim-nio',
+      },
+      {
+        'theHamsta/nvim-dap-virtual-text',
+        config = true,
+        dependencies = {
+          'mfussenegger/nvim-dap',
+        },
+      },
+      -- keep-sorted end
+    },
+  },
+  -- keep-sorted end
 }
