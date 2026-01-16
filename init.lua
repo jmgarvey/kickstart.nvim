a985f23b12668315a109c5744517dd1fc519c307
Jared Garvey <jared@medra.ai>
2026-01-15 17:24:00 -0800

    sort most recently used

--- init.lua
+++ init.lua
@@ -403,6 +403,7 @@ require('lazy').setup({
           buffers = {
             show_all_buffers = true,
             sort_lastused = true,
+            sort_mru = true,
             mappings = {
               i = {
                 ['<c-d>'] = 'delete_buffer',
