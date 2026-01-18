5abb76765c953a62688fdb5968b861db6e335561
Jared Garvey <jared@garvey.cc>
2026-01-18 10:43:24 -0800

    cleanup

--- lua/kickstart/plugins/snacks.lua
+++ lua/kickstart/plugins/snacks.lua
@@ -1,19 +1,12 @@
 return {
-  -- lazy.nvim
   {
     'folke/snacks.nvim',
+    priority = 1000,
     ---@type snacks.Config
     opts = {
-      dashboard = {
-        -- your dashboard configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      },
-      notifier = {
-        -- your notifier configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      },
+      dashboard = {},
+      notifier = {},
+      image = {},
     },
   },
 }
