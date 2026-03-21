8e08ca08be2099ae30fe9fa72f827ee14c5b3c3f
Jared Garvey <jared@medra.ai>
2026-03-21 05:23:44 -0700

    remove prettier from lsp list

--- init.lua
+++ init.lua
@@ -557,7 +557,6 @@ require('lazy').setup({
         eslint = {},
         rust_analyzer = {},
         ruff = {},
-        prettier = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
