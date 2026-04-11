dfc917e781da591516d597f232f68e449e956a8c
Jared Garvey <jared@medra.ai>
2026-04-11 07:38:50 -0700

    add grug

---
+++ lua/custom/plugins/grug-far.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    'MagicDuck/grug-far.nvim',
+    cmd = { 'GrugFar', 'GrugFarWithin' },
+    keys = {
+      {
+        '<leader>sR',
+        function()
+          require('grug-far').open {
+            transient = true,
+            instanceName = 'replace',
+            staticTitle = 'Grug Find and Replace',
+          }
+        end,
+        desc = '[S]earch [R]eplace (grug)',
+      },
+      {
+        '<leader>sW',
+        function()
+          require('grug-far').open {
+            transient = true,
+            instanceName = 'replace',
+            staticTitle = 'Grug Find and Replace',
+            prefills = { search = vim.fn.expand '<cword>' },
+          }
+        end,
+        desc = '[S]earch current [W]ord (grug)',
+      },
+      {
+        '<leader>sv',
+        function()
+          require('grug-far').open {
+            transient = true,
+            staticTitle = 'Grug Find and Replace',
+            visualSelectionUsage = 'operate-within-range',
+          }
+        end,
+        mode = { 'n', 'x' },
+        desc = '[S]earch in [V]isual range (grug)',
+      },
+    },
+    opts = {},
+  },
+}
